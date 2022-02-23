307916eb7543ac52c1fdbe64dfe0ffeed1babfa9
nick selivanov <nick.selivanov@gmail.com>
2022-02-23 10:46:33 +0300

    feature/TMPFRONT-22 исправлены конфликты

--- src/pages/Home/sections/WhatDoWant/Place/Place.tsx
+++ src/pages/Home/sections/WhatDoWant/Place/Place.tsx
@@ -1,30 +1,66 @@
-import React, {useState} from "react";
+import React, { FC, useState } from "react";
 import Input from "../Input/Input";
-import {getCities} from "../../../../../services/getCities";
-import s from './Place.module.scss';
+import { getCities } from "../../../../../services/getCities";
+import s from "./Place.module.scss";
 
-export const Place = () => {
+interface PlaceProps {
+  value?: string;
+}
 
-    const [state, setState] = useState({city: '', cities: [], edit: false});
+export const Place: FC<PlaceProps> = ({ value }) => {
 
-    const input = () => document.getElementById('place');
-    const autocomplete = () => document.getElementById('list');
-    const closeAutoComplete = (e: any) => !e.path.some((el:React.ReactNode) => el === autocomplete() || el === input()) ? setState((state) => ({...state, edit: false})) : null;
-    document.addEventListener('click', closeAutoComplete);
-    const autoCompleteClick = (e: any) => setState((state) => ({...state, city: e.target.textContent, edit: false}));
-    const autocompleteArr = state.cities.slice(0, 5).map((el: any, i) => (<span className={s.autocompleteItem} key={i} onClick={autoCompleteClick} onKeyDown={autoCompleteClick}>{el.city}</span>));
-    const autocompleteList = (!!state.cities.length && state.edit) && <div className={s.autocompleteList} id="list">{autocompleteArr}</div>;
+  const [state, setState] = useState({ city: value || "", cities: [], edit: false });
 
-    const onChangeFunc = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setState((state) => ({...state, edit: true, city: e.target.value}));
-        // /check/city/{name} GET
-       getCities('place').then((result) => result ? setState((state) => ({...state, cities: result})) : null);
-    };
+  const input = () => document.getElementById("place");
 
-    return (
-        <div className={s.autocomplete}>
-        <Input placeholder="Местоположение" icon="place" onChangeFunc={onChangeFunc} id="place" value={state.city} />
-            {autocompleteList}
-        </div>
-    );
+  const autocomplete = () => document.getElementById("list");
+
+  const closeAutoComplete = (e: any) => !e.path.some((el: React.ReactNode) => el === autocomplete()
+    || el === input()) ? setState((state) => ({
+    ...state,
+    edit: false
+  })) : null;
+
+  document.addEventListener("click", closeAutoComplete);
+
+  const autoCompleteClick = (e: any) => {
+    console.log(e.target.innerText);
+    setState((state) => ({ ...state, city: e.target.innerText, edit: false }));
+  };
+
+  const autocompleteArr = state.cities.slice(0, 5).map((el: any, i) => (
+    <span
+      className = {s.autocompleteItem}
+      key = {i}
+      onClick = {autoCompleteClick}
+      onKeyDown = {autoCompleteClick}>
+      {el.city}
+    </span>));
+
+  const autocompleteList = (!!state.cities.length && state.edit)
+    && <div className = {s.autocompleteList} id = "list">
+      {autocompleteArr}
+    </div>;
+
+  const handleChangeInputValue = (inputValue: string) => {
+    setState((state) => ({ ...state, edit: true, city: inputValue }));
+    // /check/city/{name} GET
+    getCities("place")
+      .then((result) => result ?
+        setState((state) => ({ ...state, cities: result }))
+        : null);
+  };
+
+  return (
+    <div className = {s.autocomplete}>
+      <Input
+        id = "place"
+        value = {state.city}
+        placeholder = "Местоположение"
+        icon = "place"
+        onChange = {(inputValue: string) => handleChangeInputValue(inputValue)}
+      />
+      {autocompleteList}
+    </div>
+  );
 };
